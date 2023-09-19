e4569ef07dbc797c58a3116cb9c935be9aef3ccb
hanting-cong <hanting.cong@hulu.com>
2023-09-19 15:48:51 +0800

    add changes

--- scripts/run_evaluation.sh
+++ scripts/run_evaluation.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-if [ ! -d data/MedQA-USMLE ]; then
-  cd data && ./download.sh MedQA-USMLE && cd -
-fi
-
 CUDA_VISIBLE_DEVICES=0 \
     deepspeed examples/evaluation.py \
-    --answer_type medmcqa \
-    --model_name_or_path gpt2-large \
-    --dataset_path data/MedQA-USMLE/validation \
+    --answer_type text \
+    --model_name_or_path models/Baichuan-7B \
+    --dataset_path data/damage_data/20230915/test \
     --deepspeed examples/ds_config.json \
     --inference_batch_size_per_device 1 \
-    --metric accuracy
+    --metric neg_log_likelihood
