b25c1afd48b145ddd78e27f51b60c8e44a283139
conght <nitingge@126.com>
2023-09-22 12:12:39 +0800

    add changes

--- scripts/run_evaluation_accelerator.sh
+++ scripts/run_evaluation_accelerator.sh
@@ -6,7 +6,9 @@ fi
 
 CUDA_VISIBLE_DEVICES=0 accelerate launch --config_file configs/accelerator_singlegpu_config.yaml examples/evaluation.py \
     --answer_type text \
-    --model_name_or_path models/Baichuan-7B \
+    --model_name_or_path models/Baichuan2-13B-Base \
+    --trust_remote_code True \
+    --lora_model_path output_models/finetuned_baichuan2_13b_qlora_20230920_01 \
     --dataset_path data/damage_data/20230915/test \
     --use_ram_optimized_load True \
     --deepspeed examples/ds_config.json \
@@ -14,4 +16,5 @@ CUDA_VISIBLE_DEVICES=0 accelerate launch --config_file configs/accelerator_singl
     --output_dir output_dir/accelerator_1_card \
     --inference_batch_size_per_device 1 \
     --use_accelerator_for_evaluator True \
+    --temperature 0.1 \
     --torch_dtype bfloat16
--- scripts/run_evaluation_with_lora.sh
+++ scripts/run_evaluation_with_lora.sh
@@ -5,14 +5,14 @@
 #   i.e. the one saved by ./scripts/run_finetune_with_lora.sh
 
 
-CUDA_VISIBLE_DEVICES=0 \
+CUDA_VISIBLE_DEVICES=0,1,2 \
     deepspeed examples/evaluation.py \
     --answer_type text \
-    --model_name_or_path models/Baichuan-7B \
+    --model_name_or_path models/Baichuan2-13B-Base \
     --trust_remote_code True \
-    --lora_model_path output_models/finetuned_baichuan7b_qlora_20230918_03 \
+    --lora_model_path output_models/finetuned_baichuan2_13b_qlora_20230920_01 \
     --dataset_path data/damage_data/20230915/test \
     --prompt_structure "Input: {input}" \
     --deepspeed examples/ds_config.json \
     --inference_batch_size_per_device 1 \
-    --metric neg_log_likelihood
+    --metric accuracy
