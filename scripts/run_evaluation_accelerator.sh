280a254e62086cc4842d2fe5bac36ec57585f304
conght <nitingge@126.com>
2023-09-25 17:24:23 +0800

    add changes

--- scripts/run_evaluation_accelerator.sh
+++ scripts/run_evaluation_accelerator.sh
@@ -6,13 +6,12 @@ fi
 
 CUDA_VISIBLE_DEVICES=0 accelerate launch --config_file configs/accelerator_singlegpu_config.yaml examples/evaluation.py \
     --answer_type text \
-    --model_name_or_path models/Baichuan2-13B-Base \
+    --model_name_or_path models/Baichuan-7B \
     --trust_remote_code True \
-    --lora_model_path output_models/finetuned_baichuan2_13b_qlora_20230920_01 \
     --dataset_path data/damage_data/20230915/test \
     --use_ram_optimized_load True \
     --deepspeed examples/ds_config.json \
-    --metric accuracy \
+    --metric neg_log_likelihood \
     --output_dir output_dir/accelerator_1_card \
     --inference_batch_size_per_device 1 \
     --use_accelerator_for_evaluator True \
